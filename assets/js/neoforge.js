bb4f54f45f4e198cd0f87028306f3d37368d3de3
cpw <cpw+github@weeksfamily.ca>
2023-10-30 18:30:09 -0400

    Fix installer links - let's have both visible for now, since people probably still want 20.1 too!

--- assets/js/neoforge.js
+++ assets/js/neoforge.js
@@ -1,11 +1,21 @@
 const VERSIONS_ENDPOINT = 'https://maven.neoforged.net/api/maven/versions/releases/'
 const FORGE_GAV = 'net/neoforged/neoforge'
+const LEGACY_GAV = 'net/neoforged/forge'
 const LATEST_ENDPOINT = 'https://maven.neoforged.net/api/maven/latest/version/releases/'
-const DOWNLOAD_URL = 'https://maven.neoforged.net/releases/net/neoforged/neoforge'
+const DOWNLOAD_URL = 'https://maven.neoforged.net/releases'
 //https://maven.neoforged.net/api/maven/latest/version/releases/net%2Fneoforged%2Fneoforge?filter=20.2
 async function loadLatestVersions(minecraftVersions) {
     for (const mcVersion of minecraftVersions) {
-        let currentMcVersionUrl = new URL(LATEST_ENDPOINT + encodeURIComponent(FORGE_GAV) + '?filter=' + encodeURIComponent(mcVersion));
+	let gav;
+	let fn;
+	if (mcVersion.startsWith("1.20.1")) {
+		gav = LEGACY_GAV;
+                fn = "forge";
+	} else {
+		gav = FORGE_GAV;
+                fn = "neoforge";
+	}
+        let currentMcVersionUrl = new URL(LATEST_ENDPOINT + encodeURIComponent(gav) + '?filter=' + encodeURIComponent(mcVersion));
         let versionJson;
 
         try {
@@ -21,13 +31,13 @@ async function loadLatestVersions(minecraftVersions) {
 
         if (versionJson) {
             const {version} = versionJson;
-            
-            const installerUrl = `${DOWNLOAD_URL}/${encodeURIComponent(version)}/neoforge-${encodeURIComponent(version)}-installer.jar`;
-            const changelogUrl = `${DOWNLOAD_URL}/${encodeURIComponent(version)}/neoforge-${encodeURIComponent(version)}-changelog.txt`;
-            document.querySelector("#filelist").innerHTML = `
+            const vs = `#filelist${mcVersion}`.split('.').join("");
+            const installerUrl = `${DOWNLOAD_URL}/${gav}/${encodeURIComponent(version)}/${fn}-${encodeURIComponent(version)}-installer.jar`;
+            const changelogUrl = `${DOWNLOAD_URL}/${gav}/${encodeURIComponent(version)}/${fn}-${encodeURIComponent(version)}-changelog.txt`;
+            document.querySelector(vs).innerHTML = `
                 <div class="fileinfo__header">NeoForge ${version}</div>
                 <div class="fileinfo__body">
-                <a href="${installerUrl}"><span>Latest <em>NeoForge</em> Installer</span><span>neoforge-${version}-installer.jar</span></a>
+                <a href="${installerUrl}"><span>Latest <em>NeoForge</em> Installer</span><span>${fn}-${version}-installer.jar</span></a>
                 <a href="${changelogUrl}"><span>Latest Changelog</span><span>${version}</span></a>
                 </div>
             `;
--- content/_index.md
+++ content/_index.md
@@ -10,12 +10,16 @@ description: |
     NeoForged installer files, as well as news and other information from the NeoForged project
 ---
 # NeoForge installer files
-You can find a direct link to our latest installer file below.
+You can find a direct link to our latest installer files below.
 
-{{< files "20.2" >}}
+{{< files "1.20.1" >}}
 
 Note: this file is still called forge because we're trying to maintain compatibility with launchers,
 assuming they don't hardcode things too much.
+
+## The new version is here, and still in beta
+
+{{<files "20.2" >}}
 
 # Using NeoForge for mod development
 
